6fc25b33895357f178208796f10026d3480e1931
Eugene-74 <eugenezrg@gmail.com>
2024-09-22 12:13:06 +0200

    ouverture simple des images et mis en structure des données

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-# g++ -o main.exe main.cpp `pkg-config --cflags --libs Qt5Widgets opencv4`
-g++ -fPIC main.cpp -o main.exe  `pkg-config --cflags --libs Qt5Widgets opencv4`
+# moc main.cpp -o moc_main.cpp
+g++ -fPIC main.cpp -o main.exe `pkg-config --cflags --libs Qt5Widgets opencv4`
--- main.cpp
+++ main.cpp
@@ -1,86 +1,228 @@
-#include <QApplication>
-#include <QWidget>
-#include <QPushButton>
-#include <QVBoxLayout>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <algorithm>
 
-#include <QMainWindow>
-#include <QAction>
-#include <QMenuBar>
-#include <QStatusBar>
-#include <QFile>
-#include <QTextStream>
-// #include <QIcon>
+namespace fs = std::filesystem;
 
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
-class MainWindow : public QWidget {
+class Date
+{
 public:
-    MainWindow() {
-        QVBoxLayout* layout = new QVBoxLayout(this);
+    int day;
+    int month;
+    int year;
 
-        QPushButton* button1 = new QPushButton("Bouton 1", this);
-        QPushButton* button2 = new QPushButton("Bouton 2", this);
-
-        layout->addWidget(button1);
-        layout->addWidget(button2);
-
-        connect(button1, &QPushButton::clicked, this, &MainWindow::onButton1Clicked);
-        connect(button2, &QPushButton::clicked, this, &MainWindow::onButton2Clicked);
-    }
-
-private slots:
-    void onButton1Clicked() {
-        // Code à exécuter lorsque le bouton 1 est cliqué
-        cv::Mat image = cv::imread("image.JPG");
-        if (image.empty()) {
-            std::cerr << "Erreur: Impossible de charger l'image" << std::endl;
+    Date(int d, int m, int y) : day(d), month(m), year(y) {
+        if (day < 0 or day > 31) {
+            std::cout << "jour invalide" << std::endl;
             return;
         }
-        cv::imshow("Image", image);
-        cv::waitKey(0);
+        else if (month < 0 or month > 12) {
+            std::cout << "mois invalide" << std::endl;
+            return;
+        }
+        else if (year < 0 or year > 9999) {
+            std::cout << "année invalide" << std::endl;
+            return;
+        }
     }
 
-    void onButton2Clicked() {
-        // Code à exécuter lorsque le bouton 2 est cliqué
-        std::cout << "Bouton 2 cliqué!" << std::endl;
+    void print() const {
+        std::cout << "Jour : " << day << "Mois : " << month << "Année : " << year << std::endl;
+    }
+    std::string getStringJJ_MM_AAAA() const {
+        // std::cout << "Jour : " << day << "Mois : " << month << "Année : " << year << std::endl;
+        return std::to_string(day) + "/" + std::to_string(month) + "/" + std::to_string(year);
     }
 };
 
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
+class Folders
+{
+public:
+    std::vector<std::string> folders;
 
-    QMainWindow mainWindow;
-    mainWindow.setWindowTitle("Easy Image Editor");
-    // window.resize(300, 200);
-    mainWindow.showMaximized();
-    mainWindow.show();
-    mainWindow.setWindowIcon(QIcon("resssource/icon.ico"));   
+    Folders(std::vector<std::string> a) : folders(a) {
+    }
 
-    
-   // Charger le fichier QSS
-    QFile file("style.qss");
-    file.open(QFile::ReadOnly);
-    QTextStream stream(&file);
-    QString styleSheet = stream.readAll();
-    app.setStyleSheet(styleSheet);
+    void print() const {
+        for (const std::string& valeur : folders) {
+            std::cout << valeur << " ";
+        }
+    }
+    std::string getFolderList() const {
+        std::string folderList;
+        for (const auto& folder : folders) {
+            if (!folderList.empty()) {
+                folderList += ", "; // Ajouter une virgule entre les dossiers
+            }
+            folderList += folder; // Ajouter le dossier
+        }
+        return folderList; // Retourner la chaîne complète
+    }
+    void addFolder(std::string folder) {
+        folders.push_back(folder);
+    }
+    std::vector<std::string>  getFolders(){
+        return folders;
+    }
 
-    // Crée une fenêtre principale
-    // QMainWindow mainWindow;
+};
 
-    // Crée un widget central
-    QWidget *centralWidget = new QWidget();
-    mainWindow.setCentralWidget(centralWidget);
+class ImageData
+{
+public:
+    std::string imagePath;
+    long date;
+    int id;
+    Folders folders;
 
-    // Crée un layout vertical pour le widget central
-    QVBoxLayout *layout = new QVBoxLayout();
-    centralWidget->setLayout(layout);
+    ImageData(std::string a, long d, int b, const Folders& c) : imagePath(a), date(d), id(b), folders(c) {}
 
-    // Ajouter des boutons
-    QPushButton *button1 = new QPushButton("Bouton 1");
-    QPushButton *button2 = new QPushButton("Bouton 2");
-    layout->addWidget(button1);
-    layout->addWidget(button2);
+    void print() const {
+        std::cout << "Image : " << imagePath
+            << " date : " << date
+            << " numéro : " << id
+            << " fichiers : " << folders.getFolderList();
 
-    return app.exec();
+        std::cout << std::endl;
+    }
+    std::string get() const {
+        return "Image : " + imagePath +
+            " date : " + std::to_string(date) +
+            " numéro : " + std::to_string(id) +
+            " fichiers : " + folders.getFolderList();
+    }
+    void addFolder(std::string toAddFolder) {
+        folders.addFolder(toAddFolder);
+    }
+    void addFolders(std::vector<std::string>  toAddFolders) {
+        for (size_t i = 0; i < toAddFolders.size(); ++i) {
+            folders.addFolder(toAddFolders[i]);
+        }
+    }
+    std::vector<std::string>  getFolders() {
+        return folders.getFolders();
+    }
+    std::string getImageName() {
+        std::filesystem::path filePath(imagePath);
+        std::string imageName = filePath.filename().string();
+        return imageName;
+    }
+    // Définition de l'opérateur ==
+    bool operator==(const ImageData& other) const {
+        std::filesystem::path filePath(imagePath);
+        std::string imageName = filePath.filename().string();
+
+        std::filesystem::path filePathOther(other.imagePath);
+        std::string imageNameOther = filePathOther.filename().string();
+
+        return imageName == imageNameOther;
+    }
+};
+
+
+class ImagesData
+{
+public:
+
+    std::vector<ImageData> imagesData;
+
+    ImagesData(std::vector<ImageData>  a) : imagesData(a) {}
+
+    void print() const {
+        std::cout << "Images : ";
+
+        for (const ImageData& valeur : imagesData) {
+            std::cout << valeur.get() << " ";
+        }
+
+        std::cout << std::endl;
+    }
+    void addImage(ImageData& imageD){
+        // chercher le bon element
+        // Utiliser std::find_if avec une lambda pour comparer les noms d'image
+        auto it = std::find_if(imagesData.begin(), imagesData.end(),
+            [&imageD](ImageData& imgD) {
+                return imgD.getImageName() == imageD.getImageName();
+            });
+
+        // Si l'élément est trouvé, le supprimer
+        if (it != imagesData.end()) {
+            ImageData lastImageD = *it;
+            imagesData.erase(it);
+            imageD.addFolders(lastImageD.getFolders());
+        }
+        else {
+            // si il n'existe pas encore
+            imagesData.push_back(imageD);
+        }
+    }
+
+    void removeImage(const ImageData& image){
+        imagesData.push_back(image);
+        // chercher le bon element
+        auto it = std::find(imagesData.begin(), imagesData.end(), image);
+
+        // Si l'élément est trouvé, le supprimer
+        if (it != imagesData.end()) {
+            imagesData.erase(it);
+        }
+    }
+};
+
+bool estImage(const std::string& cheminFichier);
+void listerContenu(const std::string& chemin);
+
+int main() {
+    // std::vector<std::string> fichiers = { "dossier1", "dossier2", "dossier3" };
+    // Folders folders(fichiers);
+
+    // ImageData image("/home/eugene/Documents/image.png", 128489237832, 0, folders);
+    // image.print();
+
+    std::string path = "/home/eugene/Documents";
+    listerContenu(path);
+
+    return 0;
+}
+
+void listerContenu(const std::string& chemin) {
+    ImagesData imagesData({});
+    // TODO definir image data plus tot 
+    for (const auto& entry : fs::directory_iterator(chemin)) {
+        if (fs::is_regular_file(entry.status())) {
+            if (estImage(entry.path())) {
+                std::cout << "Image: " << entry.path().filename() << std::endl;
+
+                std::vector<std::string> fichiers = { entry.path().parent_path().filename() };
+                Folders folders(fichiers);
+                ImageData image(entry.path(), 128489237832, 0, folders);
+                image.print();
+                imagesData.addImage(image);
+            }
+            // else{
+            //     std::cout << "Fichier: " << entry.path().filename() << std::endl;
+            // }
+        }
+        else if (fs::is_directory(entry.status())) {
+            listerContenu(entry.path());
+            // std::cout << "Dossier: " << entry.path().filename() << std::endl;
+        }
+    }
+    imagesData.print();
+}
+
+bool estImage(const std::string& cheminFichier) {
+    // Liste des extensions d'images courantes
+    std::vector<std::string> extensionsImages = { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp" };
+
+    // Récupération de l'extension du fichier
+    std::string extension = fs::path(cheminFichier).extension().string();
+
+    // Convertir l'extension en minuscule pour éviter les problèmes de casse
+    std::transform(extension.begin(), extension.end(), extension.begin(), ::tolower);
+
+    // Vérifier si l'extension est dans la liste des extensions d'images
+    return std::find(extensionsImages.begin(), extensionsImages.end(), extension) != extensionsImages.end();
 }
