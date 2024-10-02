8bdc0dca4338c8946e149f8aad0bd0ce3aed2449
Eugene-74 <eugenezrg@gmail.com>
2024-10-03 01:05:48 +0200

    create

--- compileLib.sh
+++ compileLib.sh
@@ -1,10 +1,18 @@
 # Compile all the library
 
+#!/bin/bash
+
+# Compile individual files
 g++ -I./functions -I./structure -I./display `pkg-config --cflags --libs opencv4` `pkg-config --cflags --libs exiv2` -c structure/date/date.cpp -o lib/date.o
 g++ -I./functions -I./structure -I./display `pkg-config --cflags --libs opencv4` `pkg-config --cflags --libs exiv2` -c structure/folders/folders.cpp -o lib/folders.o
 g++ -I./functions -I./structure -I./display `pkg-config --cflags --libs opencv4` `pkg-config --cflags --libs exiv2` -c structure/metaData/metaData.cpp -o lib/metaData.o
 g++ -I./functions -I./structure -I./display `pkg-config --cflags --libs opencv4` `pkg-config --cflags --libs exiv2` -c structure/imageData/imageData.cpp -o lib/imageData.o
 g++ -I./functions -I./structure -I./display `pkg-config --cflags --libs opencv4` `pkg-config --cflags --libs exiv2` -c structure/imagesData/imagesData.cpp -o lib/imagesData.o
+
+# Compile the rest of the files similarly...
+
+# Archive everything into a static library
+ar rcs lib/libfolders.a lib/*.o
 
 
 
@@ -23,8 +31,7 @@ g++ -c moc_imageEditor.cpp -fPIC `pkg-config --cflags --libs Qt5Widgets` -o lib/
 
 
 
-ar rcs lib/libfolders.a lib/vector.o lib/folders.o lib/date.o lib/metaData.o lib/imageData.o lib/imagesData.o lib/thumbnail.o lib/imageEditor.o lib/moc_imageEditor.o lib/clickableLabel.o
-# ar rcs lib/libfolders.a lib/*.o
+ar rcs lib/libfolders.a lib/*.o
 
 
 # sh compileLib.sh
