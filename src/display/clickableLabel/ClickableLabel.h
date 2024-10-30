5fdc2df8128d1263bdcd65c3cd1dce6142346d8c
Eugene-74 <eugenezrg@gmail.com>
2024-10-30 11:18:38 +0100

    moving clickableLabel to  it's own file

---
+++ src/display/clickableLabel/ClickableLabel.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <QLabel>
+#include <opencv2/opencv.hpp>
+#include <QFileDialog>
+#include <QMouseEvent>
+
+
+
+class ClickableLabel : public QLabel {
+    Q_OBJECT
+
+public:
+    explicit ClickableLabel(const QString& i, QWidget* parent = nullptr, QSize size = QSize(0, 0), bool setSize = true);
+
+signals:
+    void clicked();  // Signal émis lors du clic
+
+protected:
+    void enterEvent(QEvent* event) override;
+    void leaveEvent(QEvent* event) override;
+    void mousePressEvent(QMouseEvent* event) override;   // Override mouse press event
+    void mouseReleaseEvent(QMouseEvent* event) override;  // Override mouse release event
+
+
+
+
+
+private:
+    QPixmap defaultPixmap;
+
+
+    int border = 0;
+    int border_radius = 5;
+
+public:
+    void updateStyleSheet();
+
+    QString border_color = "transparent"; //#b3b3b3
+    QString hover_border_color = "#9c9c9c";
+
+
+    QString background_color = "transparent";
+    QString hover_background_color = "#b3b3b3";
+};
--- src/display/imageEditor/ImageEditor.h
+++ src/display/imageEditor/ImageEditor.h
@@ -9,7 +9,6 @@
 #include <QWidget>
 #include <QFileDialog>
 #include <QScreen>
-#include <QMessageBox>
 #include <QTimer>
 #include <iostream>
 #include <QResizeEvent>
@@ -23,45 +22,10 @@
 #include "../../display/loadImage/LoadImage.h"
 #include "../../structure/data/Data.h"
 #include "../../display/box/Box.h"
+#include "../../display/clickableLabel/ClickableLabel.h"
+
 #include "../../Const.h"
 
-
-
-class ClickableLabel : public QLabel {
-    Q_OBJECT
-
-public:
-    explicit ClickableLabel(const QString& i, QWidget* parent = nullptr, QSize size = QSize(0, 0), bool setSize = true);
-
-signals:
-    void clicked();  // Signal émis lors du clic
-
-protected:
-    void enterEvent(QEvent* event) override;
-    void leaveEvent(QEvent* event) override;
-    void mousePressEvent(QMouseEvent* event) override;   // Override mouse press event
-    void mouseReleaseEvent(QMouseEvent* event) override;  // Override mouse release event
-
-
-
-
-private:
-    QPixmap defaultPixmap;
-
-
-    int border = 0;
-    int border_radius = 5;
-
-public:
-    void updateStyleSheet();
-
-    QString border_color = "transparent"; //#b3b3b3
-    QString hover_border_color = "#9c9c9c";
-
-
-    QString background_color = "transparent";
-    QString hover_background_color = "#b3b3b3";
-};
 
 
 class ImageEditor : public QMainWindow {
