719b10579b4fc54b471f0e32ae3228b7b078b2a4
Eugene-74 <eugenezrg@gmail.com>
2024-12-21 14:17:56 +0100

    Update build configuration and remove obsolete scripts

--- compile.sh
+++ compile.sh
@@ -1,13 +1,13 @@
 unset GTK_PATH
 # Créer un répertoire de build et y naviguer
-mkdir buildLinux
+mkdir -p buildLinux
 cd buildLinux
 
-# Configurer le projet avec CMake
-cmake ..
+# Configurer le projet avec CMake en utilisant Ninja
+cmake -G Ninja ..
 
-# Compiler le projet
-cmake --build .
+# Compiler le projet avec Ninja
+ninja
 
 # Lancer l'exécutable
 ./EasyImageEditor
