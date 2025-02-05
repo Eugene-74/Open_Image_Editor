b95e1f142dda4ae61fbce7497e8ff3cf8ae036c8
Eugene-74 <eugenearg@gmail.com>
2025-02-05 10:48:42 +0100

    update release

--- include/Version.h
+++ include/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define APP_NAME "OpenImageEditor"
-#define APP_VERSION "1.0.19"
+#define APP_VERSION "1.0.20"
 #define COMPANY_NAME "Your Company"
 #define COPYRIGHT_YEAR "2023"
--- install/nsis_variables.nsh
+++ install/nsis_variables.nsh
@@ -1,4 +1,4 @@
 !define APP_NAME "OpenImageEditor"
-!define APP_VERSION "1.0.19"
+!define APP_VERSION "1.0.20"
 !define COMPANY_NAME "Your Company"
 !define COPYRIGHT_YEAR "2023"
