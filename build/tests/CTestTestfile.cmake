fb98352fc52e82c40ad13668031cede70ecbf50f
Eugene-74 <eugenearg@gmail.com>
2025-02-15 21:20:12 +0100

    Suppression de tous les fichiers de github-pages

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/eugen/Documents/MesDocuments/git/Open_Image_Editor/tests
-# Build directory: C:/Users/eugen/Documents/MesDocuments/git/Open_Image_Editor/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(OpenImageEditorTests "C:/Users/eugen/Documents/MesDocuments/git/Open_Image_Editor/build/tests/OpenImageEditorTests.exe")
-set_tests_properties(OpenImageEditorTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/eugen/Documents/MesDocuments/git/Open_Image_Editor/tests/CMakeLists.txt;59;add_test;C:/Users/eugen/Documents/MesDocuments/git/Open_Image_Editor/tests/CMakeLists.txt;0;")
