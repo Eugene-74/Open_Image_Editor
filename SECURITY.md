bea5d40d775a8bbeb62ade02446d395bf94d466a
Eugene-74 <eugenearg@gmail.com>
2025-03-12 16:32:57 +0100

    Initial commit sur la nouvelle branche vide

--- SECURITY.md
+++
@@ -1,7 +0,0 @@
-# Security Policy
-
-## Supported Versions
-Only the last version should not have bugs
-
-## Reporting a Vulnerability
-For now you can't report bugs
--- docs/search/all_8.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['person_0',['Person',['../class_person.html',1,'']]],
-  ['predeleteimage_1',['preDeleteImage',['../class_data.html#a3000024fbd81870872d3b36cca543418',1,'Data']]]
-];
