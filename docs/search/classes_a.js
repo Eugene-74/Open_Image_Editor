f50136ab521f1e7a5d4af73bb05ec4ec49e28243
eureur74 <eugene.raveneau@gmail.com>
2025-03-12 19:10:17 +0100

    Update documentation

--- docs/search/classes_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['rotation_0',['Rotation',['../class_const_1_1_rotation.html',1,'Const']]]
-];
--- scripts/generateDoc.bat
+++ scripts/generateDoc.bat
@@ -1,6 +1,6 @@
 @echo off
 
-xcopy "%USERPROFILE%\Downloads\docs" docs /E /I /Y
+xcopy "%USERPROFILE%\Downloads\docs" . /E /I /Y
 
 git add .
 git commit -m "Update documentation"
