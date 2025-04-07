03cfde155fbfc41059d13cc01eb76d2be79b6eb5
Eugene-74 <eugenezrg@gmail.com>
2025-04-07 11:17:49 +0200

    empty branch

--- CTestConfig.cmake
+++
@@ -1,3 +0,0 @@
-# Set the dashboard root
-set(CTEST_PROJECT_NAME "OpenImageEditor")
-set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
--- tests/TestConst.hpp
+++
@@ -1,3 +0,0 @@
-#include "Const.hpp"
-
-const QString TESTS_PATH = DOCUMENTS_PATH + "/MesDocuments/git/Open_Image_Editor/tests";
