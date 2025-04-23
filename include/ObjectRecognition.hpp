8ca6b504ae457eb6af93ab8dc845f7594c271e9a
Eugene-74 <eugenezrg@gmail.com>
2025-04-23 13:19:23 +0200

    changing name

---
+++ include/ObjectRecognition.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+// #include <dlib/opencv.h>
+#include <opencv2/face.hpp>
+#include <opencv2/opencv.hpp>
+
+// Forward declaration
+class QImage;
+class Data;
+
+class DetectedObjects {
+   private:
+    std::map<std::string, std::vector<std::pair<cv::Rect, float>>> detectedObjects;
+
+   public:
+    void save(std::ofstream& out) const;
+    void load(std::ifstream& in);
+    void clear();
+    std::map<std::string, std::vector<std::pair<cv::Rect, float>>> getDetectedObjects();
+    std::map<std::string, std::vector<std::pair<cv::Rect, float>>> getDetectedObjectsConst() const;
+
+    void setDetectedObjects(const std::map<std::string, std::vector<std::pair<cv::Rect, float>>>& detectedObjects);
+};
+
+bool isCudaAvailable();
+void detectObjectsAsync(Data* data, std::string imagePath, QImage image, std::function<void(DetectedObjects)> callback);
+// DetectedObjects detect(std::string imagePath, QImage image);
+void computeFaces(Data* data, std::string imagePath);
+std::vector<std::string> loadClassNames(const std::string& filename);
+cv::Mat QImageToCvMat(const QImage& inImage);
+QImage CvMatToQImage(const cv::Mat& inImage);
