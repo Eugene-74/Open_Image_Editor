86c80068ce25c0c6fd0ea6e047bda636a1d24586
Eugene-74 <eugenezrg@gmail.com>
2025-06-05 09:20:39 +0200

    dynamic doc version

--- scripts/generateDoc.bat
+++ scripts/generateDoc.bat
@@ -1,7 +1,13 @@
 @echo off
 
 xcopy "%USERPROFILE%\Downloads\docs" . /E /I /Y
-
+for /f "usebackq tokens=*" %%v in ('dir /b /ad docs') do (
+    findstr /c:"\"%%v\"" versions.json >nul
+    if errorlevel 1 (
+        powershell -Command "(Get-Content versions.json | ConvertFrom-Json) + '%%v' | Set-Content versions.json"
+        powershell -Command "((Get-Content versions.json | ConvertFrom-Json) | Sort-Object) | ConvertTo-Json | Set-Content versions.json"
+    )
+)
 git add .
 git commit -m "Update documentation"
 git push origin gh-pages
