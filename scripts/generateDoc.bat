6f7aae7e3de4afa73d1ece0a5835b10a75360f4a
Eugene-74 <eugenezrg@gmail.com>
2025-06-05 09:41:24 +0200

    Update documentation

--- scripts/generateDoc.bat
+++ scripts/generateDoc.bat
@@ -4,6 +4,8 @@ xcopy "%USERPROFILE%\Downloads\docs" . /E /I /Y
 
 @REM TODO faire versio auto
 
+python "%~dp0update_versions_json.py"
+
 git add .
 git commit -m "Update documentation"
 git push origin gh-pages
